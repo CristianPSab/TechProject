97546be0c1e89eb803f475ab1fb55efbd5c9f79f
CristianPSab <crpunti.sab@hotmail.com>
2023-06-07 16:12:12 +0200

    Read File completed closes #3

---
+++ Tech/Controller/FileController.cs
@@ -0,0 +1,70 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tech.Model;
+using Tech.View;
+
+namespace Tech.Controller
+{
+    public class FileController
+    {
+        private readonly FileModel _model;
+        private readonly DataViewManager _view;
+
+        public FileController(FileModel model, DataViewManager view)
+        {
+            _model = model;
+            _view = view;
+        }
+
+     
+
+        public List<FileModel>  ReadFileCSV ()
+        {
+            CultureInfo ci = new CultureInfo("es-Us");
+            Console.WriteLine("Introduzca el nombre o ruta del archivo");
+            var path = Console.ReadLine();
+            try
+            {
+                using (          
+                    
+                    var streamReader = new StreamReader(path))
+                {
+                    var csvConfig = new CsvConfiguration(cultureInfo: ci)
+                    {
+                        Delimiter = ";"
+                    };
+
+                    using (var csvReader = new CsvReader(streamReader, csvConfig))
+                    {
+
+                        csvReader.Context.RegisterClassMap<MapController>();
+                        return csvReader.GetRecords<FileModel>().ToList();
+
+                        
+                    }
+
+
+
+                  }
+                 }catch (FileNotFoundException ex) {
+                        throw new FileNotFoundException(ex + "No se puede leer el archivo debido a un problema");
+            }
+
+            }
+        }
+            
+     
+    
+}
---
+++ Tech/Controller/MapController.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech.Model;
+
+namespace Tech.Controller
+{
+    public class MapController  : ClassMap<FileModel>
+    {
+        public MapController() 
+        {
+           
+            Map(m => m.Date).Name("Fecha");
+            Map(m => m.Cierre).Name("Cierre");
+            Map(m => m.Apertura).Name("Apertura");
+        }
+    }
+}
